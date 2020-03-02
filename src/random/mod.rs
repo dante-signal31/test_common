f86a3aa7030bd28e7aaaab2d2b53c20d717f59c1
dante-signal31 <dante.signal31@gmail.com>
2020-03-02 22:59:50 +0100

    Added function to generate random strings.

---
+++ src/random/mod.rs
@@ -0,0 +1,3 @@
+/// Package to perform random operations.
+
+pub mod strings;
---
+++ src/random/strings.rs
@@ -0,0 +1,25 @@
+use rand::prelude::*;
+use rand::distributions::Alphanumeric;
+
+fn random_string(len: usize)-> String {
+    let generated_string: String = rand::thread_rng()
+        .sample_iter(Alphanumeric)
+        .take(len)
+        .collect();
+    generated_string
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_random_string() {
+        let desired_length: usize = 7;
+        let generated_string = random_string(desired_length);
+        let generated_length = generated_string.len();
+        assert_eq!(desired_length, generated_length,
+                   "Generated random string has not desired length of {} but {} instead",
+                   desired_length, generated_length);
+    }
+}
